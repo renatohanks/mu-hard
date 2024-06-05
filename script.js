f7f6068f847a2b531e5d966ebd2e4774500da8c5
renatohanks <renatohanks@users.noreply.github.com>
2024-06-05 10:49:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 function copy(){
+    var txt = document.querySelector(".listacode");
+    var text = document.querySelector(".text");
     document.querySelector(".text").innerHTML = "Copiado!";
+    txt.style.background = "grey";
+    text.style.color = "rgba(228, 224, 34, 0.829)";
+    
 }
 
 function copy2(){
+    var txt = document.querySelector(".listacode2");
+    var text2 = document.querySelector(".text2");
     document.querySelector(".text2").innerHTML = "Copiado!";
+    txt.style.background = "grey";
+    text2.style.color = "rgba(228, 224, 34, 0.829)";
 }
 
 function copy3(){
+    var txt = document.querySelector(".listacode3");
+    var text3 = document.querySelector(".text3");
     document.querySelector(".text3").innerHTML = "Copiado!";
+    txt.style.background = "grey";
+    text3.style.color = "rgba(228, 224, 34, 0.829)";
 }
 
 function copy4(){
+    var txt = document.querySelector(".listacode4");
+    var text4 = document.querySelector(".text4");
     document.querySelector(".text4").innerHTML = "Copiado!";
+    txt.style.background = "grey";
+    text4.style.color = "rgba(228, 224, 34, 0.829)";
 }
 function copy5(){
+    var txt = document.querySelector(".listacode5");
+    var text5 = document.querySelector(".text5");
     document.querySelector(".text5").innerHTML = "Copiado!";
+    txt.style.background = "grey";
+    text5.style.color = "rgba(228, 224, 34, 0.829)";
 }
 
 function abrirModal(){
